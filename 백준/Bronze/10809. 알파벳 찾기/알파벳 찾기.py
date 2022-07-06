3c65305489e6ca66db8523bae8b2898b69c8460a
Hyun Bin <83809713+smaragdine-hb@users.noreply.github.com>
2022-07-06 23:41:36 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,7 @@
+a = ['a', 'b' ,'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+word = str(input())
+for i in a:
+    if i in word:
+        print(word.index(i), end=' ')
+    else:
+        print("-1", end=' ')
